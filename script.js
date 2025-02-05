cbb15f89f9753c1fa0bf088dffaef7706aa1cbd8
Trinav <trinav.dhakal1@gmail.com>
2025-02-04 18:23:36 -0600

    Add a function to take computer input
    Add the function that generates random computer input

---
+++ script.js
@@ -0,0 +1,25 @@
+function getComputerChoice(){
+    let randomNum = Math.floor((Math.random()*3) +1);
+    switch(randomNum) {
+        case 1:
+            return "rock";
+                    
+        case 2:
+            return "paper";
+                        
+        case 3:
+            return "scissors";
+                   
+    }
+}
+
+
+
+// Create a function called getHumanChoice.
+    //Ask the user for an input and store it in userChoice
+    //See if user choice is valid or not.
+    //If valid go ahead, else ask again until valid
+    // return the valid choice
+
+
+
